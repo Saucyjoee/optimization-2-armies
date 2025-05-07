88482fd1e64d454a0d22f41468a014be2d79cde5
marcusbru1 <49483317+marcusbru1@users.noreply.github.com>
2025-05-07 17:40:35 +0200

    apply_strategy

--- verify.py
+++ verify.py
@@ -21,3 +21,13 @@ def winner(num):
 payoff = np.matrix([[sum([winner(strategy[y][i] - strategy[x][i]) for i in range(3)]) for y in range(36)] for x in range(36)])
 
 print(payoff)
+
+def apply_strategy(M, Probs):
+    #Probs is a list of size equal to the total strategy's
+    for p in range(len(Probs)):
+        M[p] *= Probs[p]
+    return M
+
+#print(apply_strategy(apply_strategy(payoff, [0 for i in range(36)]).transpose(), [1 for i in range(36)]))
+
+
